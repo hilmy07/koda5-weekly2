6d750a94a184a9739a2884629455f0dcb7d3cf29
hilmy <haidaraly98@gmail.com>
2025-11-16 11:26:08 +0700

    feat[Hilmy]: add feature range of number function

---
+++ tasks/hilmy/rentang.js
@@ -0,0 +1,20 @@
+// Range of Numbers
+
+/**
+ * Function to create an array containing a range of numbers from a to b
+ * @param {number} a - The starting number of the range
+ * @param {number} b - The ending number of the range
+ */
+
+export function buatRentang(a, b){
+    let rentang = [];
+    let row = '';
+
+    for(let i=a; i<=b; i++){
+        rentang.push(i);
+    }
+    console.log(rentang);
+
+    return row
+}
+
